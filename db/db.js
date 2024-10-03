2538fc436d64805d187c3152fbed389dec96df5e
Leonardo <leoonardoogzz@gmail.com>
2024-10-03 15:47:15 -0600

    Leito vos podes

--- db/db.js
+++ db/db.js
@@ -1,8 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const conexion = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "crud_node",
 });
+
+conexion.connect((error) => {
+  if (error) {
+    console.log("hubo un error al conectarse en la base de datos" + error);
+    return;
+  }
+
+  console.log("Se connecto exitosamente a la base de datos");
+});
+
+module.exports = conexion;
